5850cdf45525a1ad629696e37b9db9a876873767
Sergey-Yudin74 <servalu@yandex.ru>
2024-09-01 10:15:53 +0300

    Module_2.3.py

--- module_2.3.py
+++ module_2.3.py
@@ -2,15 +2,12 @@ numbers = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 numbers_ = 0
 
-while len(numbers) > numbers_:
+while numbers_ < len(numbers):
     
-    if numbers[numbers_] > 0:
-        
-        print(numbers[numbers_])
-          
-    numbers_ += 1
-
-    if numbers[numbers_] < 0:
-        
+    if numbers[numbers_] <  0:
         break
-         
+    else:
+       if numbers[numbers_] >  0:
+        print(numbers[numbers_])
+    numbers_ += 1
+    continue
